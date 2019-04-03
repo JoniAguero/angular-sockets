abddebd92c82d6ea7b2957940f3f7691f5580eb3
Joni Agüero <aguerojonathanivan@gmail.com>
2019-04-03 15:32:25 -0300

    Enviando msj desde el servidor

--- server/routes/router.ts
+++ server/routes/router.ts
@@ -6,26 +6,35 @@ const router = Router();
 router.get('/mensajes', (req:Request, res: Response) => {
     res.json({ok: true, mensaje: 'tutu bem GET!'})
 })
+
 router.post('/mensajes', (req:Request, res: Response) => {
 
-    const {cuerpo, de} = req.body;
+    const {body, de} = req.body;
 
-    res.json({ok: true, mensaje: 'tutu bem POST!', cuerpo, de})
+    const payload = {
+        body,
+        de
+    }
+
+    const server = Server.instance;
+    server.io.emit('new-message', payload)
+    res.json({ok: true, body, de})
 })
+
 router.post('/mensajes/:id', (req:Request, res: Response) => {
 
-    const {cuerpo, de} = req.body;
+    const {body, de} = req.body;
     const id = req.params.id;
 
     const payload = {
-        cuerpo,
+        body,
         de
     } 
 
     const server = Server.instance;
     server.io.in(id).emit('message-private', payload)
 
-    res.json({ok: true, mensaje: 'tutu bem POST!', cuerpo, de, id})
+    res.json({ok: true, mensaje: 'tutu bem POST!', body, de, id})
 })
 
 export default router;
